28223d8aebc78bb3a5d56831bdab54c5b061fb92
Raphael de Faria Rosa <99773236+RaphaelRosa74@users.noreply.github.com>
2025-06-11 16:01:50 -0300

    projeto com firebase criado

---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw4rU2_37aK2igxjlt12KzjfADxdfE9DQ",
+  authDomain: "miniblog-5f95a.firebaseapp.com",
+  projectId: "miniblog-5f95a",
+  storageBucket: "miniblog-5f95a.firebasestorage.app",
+  messagingSenderId: "138715671127",
+  appId: "1:138715671127:web:1c31816cd7cab6bc2d9112"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
