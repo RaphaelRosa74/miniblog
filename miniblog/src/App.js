21b1a857654bc97c19f3a9592c7e9697302aeb3f
Raphael de Faria Rosa <99773236+RaphaelRosa74@users.noreply.github.com>
2025-07-01 19:49:25 -0300

    inicio do projeto add um pouco de styles

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -6,16 +6,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
 
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
       <div className='container'>
         <Routes>
           <Route path='/' element={ < Home /> }/>
           <Route path='/about' element={ <About /> }/>
         </Routes>
       </div>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
